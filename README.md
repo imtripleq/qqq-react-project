3bbf3edbd44428ba27b1a779f9485eca332bc70c
imtripleq <flamework89@hotmail.com>
2021-12-23 21:49:45 +1100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # qqq-react-project
+
+# Dennis Khor React Project
+
+## This is a on-going project
+
+dated 23/12/2021
+
+I'm currently still working on this project.
--- src/data.js
+++ src/data.js
@@ -46,58 +46,58 @@ export const categories = [
 export const products = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-photo/pair-trainers_144627-3799.jpg",
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-photo/brown-leather-shoes_1203-7562.jpg",
   },
   {
     id: 3,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://as1.ftcdn.net/v2/jpg/01/21/81/86/1000_F_121818673_6EID5iF76VCCc4aGOLJkd94Phnggre3o.jpg",
   },
   {
     id: 4,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-psd/black-t-shirt-mockup_125540-430.jpg",
   },
   {
     id: 5,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-psd/black-hoodie-right-side-with-zipper-mockup_125540-695.jpg",
   },
   {
     id: 6,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-psd/back-side-black-hoodie-mockup_125540-693.jpg",
   },
   {
     id: 7,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-psd/black-front-sweater-mockup_125540-570.jpg",
   },
   {
     id: 8,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://img.freepik.com/free-psd/black-back-sweater-mockup_125540-569.jpg?size=338&ext=jpg",
   },
   {
     id: 9,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-psd/blue-front-sweater-mockup_125540-571.jpg",
   },
   {
     id: 10,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-photo/sunglasses_1203-7886.jpg",
   },
   {
     id: 11,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-photo/sunglasses_1203-8703.jpg",
   },
   {
     id: 12,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://img.freepik.com/free-photo/socks-new-striped-elegant-color_1203-6419.jpg?size=338&ext=jpg",
   },
   {
     id: 13,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-photo/socks-isolated_1203-6958.jpg",
   },
   {
     id: 14,
-    img: "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://image.freepik.com/free-photo/white-long-sleeve-t-shirt-men-s-basic-wear-full-body_53876-102836.jpg",
   },
 ];
